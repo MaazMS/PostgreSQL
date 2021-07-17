e4af2462cd434f671e3ec0c6280b6679213d1973
MaazMS <maazshaikh7020@gmail.com>
2021-07-17 09:26:57 +0530

    update record of DB

---
+++ gorm_basic/update_record/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "p"
+	dbname   = "users"
+)
+
+type UserInfo struct {
+	Id         int
+	First_name string
+	Last_name  string
+	Age        int
+}
+
+func main() {
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := gorm.Open(postgres.Open(psqlconn), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the Database")
+	}
+
+	// take the address of 'UserInfo{}'
+	db.AutoMigrate(&UserInfo{})
+
+	user := UserInfo{
+		Id:         2,
+		First_name: "MaazMS",
+		Last_name:  "Shaikh",
+		Age:        23,
+	}
+	// take the address of variable UserInfo, also have id to which value to update
+	db.Updates(&user)
+
+	fmt.Println("update record")
+
+}
